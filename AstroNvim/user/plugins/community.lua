80d990ebb8fc10855ad640cf1d2820d0916ffe35
tiezhu <tiezhu@study.com>
2023-12-06 20:23:38 +0800

    refactor(commu): order in community.lua

--- AstroNvim/user/plugins/community.lua
+++ AstroNvim/user/plugins/community.lua
@@ -12,13 +12,13 @@ return {
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.indent.mini-indentscope" },
   -- lsp
-  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.lsp.lsp-signature-nvim" }, -- parameter prompt
   -- Debug
-  { import = "astrocommunity.debugging.persistent-breakpoints-nvim" },
   { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
+  { import = "astrocommunity.debugging.persistent-breakpoints-nvim" },
   -- tools
   { import = "astrocommunity.motion.flash-nvim" }, -- easy motion
   { import = "astrocommunity.editing-support.todo-comments-nvim" }, -- TODO
