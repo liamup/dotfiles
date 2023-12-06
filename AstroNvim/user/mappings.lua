c25995e456328fa77d53324ba58c20c78e2f3a25
tiezhu <tiezhu@study.com>
2023-12-06 17:40:02 +0800

    refactor(cmdline): simplify mappings

--- AstroNvim/user/mappings.lua
+++ AstroNvim/user/mappings.lua
@@ -4,9 +4,6 @@ return {
     -- second key is the lefthand side of the map
 
     -- mappings Enter to open fine-cmdline
-    ["<CR>"] = {
-      function() vim.api.nvim_command ":FineCmdline" end,
-      desc = "fine cmdline",
-    },
+    ["<CR>"] = { "<cmd>FineCmdline<CR>", desc = "fine cmdline" },
   },
 }
