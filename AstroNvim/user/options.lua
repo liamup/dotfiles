7eba7fa109d4801ba8bf7936823331bbd5a91061
tiezhu <tiezhu@study.com>
2023-12-06 22:18:17 +0800

    feat(tab): set shiftwidth and tabstop to 4

---
+++ AstroNvim/user/options.lua
@@ -0,0 +1,10 @@
+-- set vim options here (vim.<first_key>.<second_key> = value)
+
+return {
+  opt = {
+    expandtab = true,
+    shiftwidth = 4,
+    smartindent = true,
+    tabstop = 4,
+  },
+}
