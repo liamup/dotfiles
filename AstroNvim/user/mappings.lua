1e08ab49b34846d0c0b197649eb04f07939fab23
tiezhu <tiezhu@study.com>
2023-12-06 20:03:52 +0800

    feat(neoconf): add mapping of neoconf

--- AstroNvim/user/mappings.lua
+++ AstroNvim/user/mappings.lua
@@ -5,5 +5,10 @@ return {
 
     -- mappings Enter to open fine-cmdline
     ["<CR>"] = { "<cmd>FineCmdline<CR>", desc = "fine cmdline" },
+
+    -- Neoconf
+    ["<leader>lg"] = { "<cmd>Neoconf<CR>", desc = "Select local/global neoconf config" },
+    ["<leader>lc"] = { "<cmd>Neoconf show<CR>", desc = "Show neoconf merge config" },
+    ["<leader>lp"] = { "<cmd>Neoconf lsp<CR>", desc = "Show neoconf merge lsp config" },
   },
 }
