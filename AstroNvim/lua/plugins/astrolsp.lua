f714e040ab7a43657d0c933986a11449c128f6fb
tiezhu <tiezhu@study.com>
2025-03-30 23:12:10 +0800

    feat(astronvim-lsp):change pyright to basedpyright

--- AstroNvim/lua/plugins/astrolsp.lua
+++ AstroNvim/lua/plugins/astrolsp.lua
@@ -8,19 +8,19 @@ return {
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
-      -- pyright = {
-      --   root_dir = function(fname)
-      --     local root_files = {
-      --       "main.py",
-      --       "setup.py",
-      --       "setup.cfg",
-      --       "requirements.txt",
-      --       "pyproject.toml",
-      --       "pyrightconfig.json",
-      --     }
-      --     return utils.find_git_ancestor(fname) or utils.root_pattern(unpack(root_files))(fname)
-      --   end,
-      -- },
+      basedpyright = {
+        root_dir = function(fname)
+          local root_files = {
+            "main.py",
+            "setup.py",
+            "setup.cfg",
+            "requirements.txt",
+            "pyproject.toml",
+            "pyrightconfig.json",
+          }
+          return utils.find_git_ancestor(fname) or utils.root_pattern(unpack(root_files))(fname)
+        end,
+      },
     },
   },
 }
